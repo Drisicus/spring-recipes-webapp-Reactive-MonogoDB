7f1efb3c10ac5f65458bf3ebe5348c2a96ff7fba
Drisicus <aaasssccc@gmail.com>
2020-11-08 14:40:21 +0100

    Updating exception handler of RecipeController

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -10,9 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
+import org.thymeleaf.exceptions.TemplateInputException;;
 
 
 @Slf4j
@@ -83,19 +81,13 @@ public class RecipeController {
         return "redirect:/";
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception){
-//
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+        model.addAttribute("exception", exception);
+        return "404error";
+    }
 
 }
